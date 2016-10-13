a6498560df9bdbce66ebfe4a10d25db9e8aa8437
Rowan Lewis <Rowan Lewis>
2016-10-13 14:56:21 +0100

    added comments to clarify how items are defined

--- items.py
+++ items.py
@@ -1,8 +1,11 @@
 item_camera = {
+
+#declares an id string for item, ideally one word
 	"id": "camera",
-
+#the way the item is referred to in-game, so that it could fit in a sentence
 	"name": "a rusty, antique camera",
-
+#the description of the item when examined (possibly contains hints for how to use in some cases?)
 	"desc": """This camera looks like it's been left untouched for
 	a long time. It's surprising it even works at all, to be honest."""
 }
+
