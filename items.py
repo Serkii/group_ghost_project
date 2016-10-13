bda6f10b65c4a72cbf49c1448e683d516c2b8dfb
Rowan Lewis <Rowan Lewis>
2016-10-13 14:35:35 +0100

    Testing repository by adding an items file with a 'camera' item defined in it.

---
+++ items.py
@@ -0,0 +1,8 @@
+item_camera = {
+	"id": "camera",
+
+	"name": "a rusty, antique camera",
+
+	"desc": """This camera looks like it's been left untouched for
+	a long time. It's surprising it even works at all, to be honest."""
+}
