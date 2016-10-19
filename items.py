8e8e07be90d8771c87082988a97e9df5207d8aa8
B-Holden <HoldenB1@cardiff.ac.uk>
2016-10-19 12:25:23 +0100

    added some items

--- items.py
+++ items.py
@@ -40,9 +40,7 @@ item_garlic = {
 
 	"desc": "A side of garlic bread included with the order you are to deliver. It is said that garlic has properties that help defend against certain supernatural aspirations, although I doubt your customers would be too happy if you lost them.",
 
-	"movable": True,
-	
-	"defense": 1.5
+	"movable": True
 }
 
 item_proton_gun = {
@@ -83,12 +81,69 @@ item_book = {
 	
 	"id": "book",
 
-	"name": "A book",
+	"name": "A Childrens book",
 
-	"desc": "The title on the book hints it would be quite boring to read that. Instead of reading it, is there another use for that?",
+	"desc": "The title on the book hints it would be quite boring to read. Instead of reading it, is there another use for that?",
 
 	"movable": True
 
-
+	#used against the toy soldiers, place in the bookshelf
 
 }
+
+item_dogcollar = {
+
+	"id": "dog_collar",
+
+	"name": "A Dog Collar",
+
+	"desc": "The Collar is little more than a simple red band, there is a name tag on it but it is too worn to read",
+
+	"movable": True
+
+	#dropped by Dog, Give to Chef for peacefull resolution
+
+}
+
+item_brooch = {
+
+	"id": "brooch",
+
+	"name": "Antique Brooch",
+
+	"desc": "A worn silver broach, the picture of a women is carved in stone in the centre with small encrusted diamonds surrounding it",
+
+	"movable": True
+
+	#Dropped by Pinkerton, Useable against the lady of the house
+
+}
+
+item_laudanum = {
+
+	"id": "laudanum",
+
+	"name": "Laudanum",
+
+	"desc": "A liquid swirls in an orange flask, the label is worn but you can just make out the word LAUDANUM on the side. It might calm your nerves but there may be side effects",
+
+	"movable": True
+
+	#Found in the Dining Room, Recovers Sanity but lowers combat skill
+
+}
+
+item_ham = {
+
+	"id": "ham",
+
+	"name": "Slices of Ham",
+
+	"desc": "Although the Mansion is ages old the ham looks surprisingly fresh, it has a ghostly chill when holding but you are hungry and food always calms you down",
+
+	"movable": True
+
+	#Found in the Kitchen, Recovers Sanity
+
+}
+
