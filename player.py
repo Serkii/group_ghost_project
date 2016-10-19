a856f1f4ea935e6383cf728a09759da98bec843a
DavidBuchanan314 <d@vidbuchanan.co.uk>
2016-10-19 21:49:48 +0100

    sound fix???

--- player.py
+++ player.py
@@ -13,5 +13,13 @@ sanity = 100
 defense_multiplier = 1.0 # lower is better
 attack_multiplier = 1.0 # higher is better
 
-player_combat_skill = 7
-
+def calculate_stats():
+	defense_multiplier = 1.0
+	attack_multiplier = 1.0
+	for item in inventory:
+		if "attack" in item:
+			attack_multiplier *= item["attack"]
+		if "defense" in item:
+			defense_multiplier *= item["defense"]
+			
+calculate_stats()
