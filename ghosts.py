16ceb906c44c53fcdcd332fd056f031a8092772f
Rowan Lewis <Rowan Lewis>
2016-10-19 12:13:04 +0100

    Prevented ghosts.py from importing the file I just deleted.

--- ghosts.py
+++ ghosts.py
@@ -1,4 +1,4 @@
-from combatscenarios import *
+
 
 def converse(ghost):
 	print("You approach %s." % ghost["name"])
@@ -24,34 +24,7 @@ def do_response(response):
 				return
 			print("Could you repeat that? (Note: This part is case-sensitive because I'm lazy)")
 
-test_ghost = {
 
-	"id": "testghost",
-
-	"name": "Spookman, the Test Ghost",
-
-	"desc": """It's a person with a white sheet over their head. The letters 'WIP' are
-	painted on the front of the sheet, underneath the eye-holes. Spooky.""",
-
-	"intro": "I AM THE TEST GHOST. YOU DON'T STAND A GHOST OF A CHANCE AGAINST ME.",
-
-	"intro2": "AH, YOU HAVE RETURNED! WHAT'S WRONG? YOU LOOK LIKE YOU'VE SEEN A GHOST!",
-
-	"player_escaped": False,
-
-	"combat_skill": 7,
-
-	"hp": 100,
-
-	"peace_conditions_met": 0,
-
-	"damage_text": "Your shot hits the test ghost! It hurts him a bit.",
-
-	"onhit_text": "The ghost punches you in the face, and giggles to itself.",
-
-	"death_text": "The ghost exclaims a surprisingly deadpan 'ouch', then falls over dead. Was it really a ghost?"
-	
-}
 
 ghost_dog = {
 
